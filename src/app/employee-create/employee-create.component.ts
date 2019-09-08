d5b9cf102ea17a48715ce4b98d8721ec47d3fe7a
Eddie <mreddiedu@gmail.com>
2019-09-07 22:05:59 -0700

    second commit

---
+++ src/app/employee-create/employee-create.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestApiService } from "../shared/rest-api.service";
+
+@Component({
+  selector: 'app-employee-create',
+  templateUrl: './employee-create.component.html',
+  styleUrls: ['./employee-create.component.css']
+})
+export class EmployeeCreateComponent implements OnInit {
+
+  @Input() employeeDetails = { dropoff_address: '', pickup_address: ''}
+
+  constructor(
+    public restApi: RestApiService, 
+    public router: Router
+  ) { }
+
+  ngOnInit() { }
+
+  addEmployee(dataEmployee) {
+    this.restApi.createEmployee(this.employeeDetails).subscribe((data: {}) => {
+      this.router.navigate(['/employees-list'])
+    })
+  }
+
+}
---
+++ src/app/shared/employee.ts
@@ -0,0 +1,4 @@
+export class Employee {
+   dropoff_address: string;
+   pickup_address: string;
+}
